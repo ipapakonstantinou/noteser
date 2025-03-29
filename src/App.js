f26521d300c86f25fe8e45b8e412df6964b12f8f
thetechjon <thetechjon@gmail.com>
2025-03-29 19:18:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NotesApp from './components/NotesApp';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Notes App</h1>
+      <NotesApp />
+    </div>
+  );
+};
+
+export default App;
